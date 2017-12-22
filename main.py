5fa66ab004443c37ea1daa760d3bd16b2a93df81
Al Azif <alazif@miskatonic.io>
2017-12-21 16:43:23 -0800

    Reduce Complexity

--- main.py
+++ main.py
@@ -5,58 +5,87 @@
 """
 
 from __future__ import print_function
-import csv
 import argparse
+import csv
 import os
 import sys
 import time
 import dropbox
 
+
 with open('oauth.conf', 'rb') as oauth:
     DBX = dropbox.Dropbox(oauth.read())
 
 
-def exists(path):
+def db_exists(path):
     """Check to see if there is a file at the given path on Dropbox"""
     try:
         DBX.files_get_metadata(path)
         return True
-    except:
+    except Exception:
         return False
 
 
-def main(path, dest, interval, sleep, kickme):
+def db_async_complete(async_id):
+    """Check to see if the async succeeded"""
+    return DBX.files_save_url_check_job_status(async_id).is_complete()
+
+
+def db_async_failed(async_id):
+    """Check to see if the async failed"""
+    return DBX.files_save_url_check_job_status(async_id).is_failed()
+
+
+def kickme():
+    """If KICK_TICK >= KICK_MAX resart the script"""
+    if KICK_TICK >= KICK_MAX:
+        print('\033[1m\033[91mKicking Script\033[0m...\033[0m')
+        os.execv(sys.executable, ['python'] + sys.argv)
+        exit()
+
+
+def main(path, dest, interval, sleep):
     """Main Method"""
+    global KICK_TICK
     with open(path, 'rb') as csvfile:
         reader = csv.DictReader(csvfile, delimiter='\t')
         for row in reader:
             title = row['Name'] + ' (' + row['Region'] + ')'
             filename = row['Content ID']
 
-            skipped = True
-            if row['PKG direct link'] != 'MISSING' and row['PKG direct link'] != '' and row['PKG direct link'] != 'CART ONLY':
-                if not exists(dest + filename + '.pkg'):
-                    aid = DBX.files_save_url(
-                        dest + filename + '.pkg',
-                        row['PKG direct link']
-                    )
-                    skipped = False
-            if skipped:
-                print('Skipped Downloading: ' + title)
+            if row['PKG direct link'] != 'MISSING' \
+               and row['PKG direct link'] != 'CART ONLY' \
+               and row['PKG direct link'] != '':
+                valid = True
             else:
-                print('Downloading ' + title + '...')
-                kicktick = 0
-                while not exists(dest + filename + '.pkg') and not DBX.files_save_url_check_job_status(aid.get_async_job_id()).is_complete():
-                    if kicktick >= kickme:
-                        print(os.linesep + '\x1b[1;31mKicking Script...\x1b[0m')
-                        os.execv(sys.executable, ['python'] + sys.argv)
-                        exit()
-                    kicktick = kicktick + 1
+                valid = False
+
+            if valid and not db_exists(dest + filename + '.pkg'):
+                aid = DBX.files_save_url(
+                    dest + filename + '.pkg',
+                    row['PKG direct link']
+                )
+                async_id = aid.get_async_job_id()
+                skipped = False
+            else:
+                skipped = True
+
+            if skipped:
+                print('\033[1mSkipped Downloading:\033[0m \033[92m' + title + '\033[0m')
+
+            if skipped and not valid:
+                print('\033[1mSkipped Downloading:\033[0m \033[91m' + title + '\033[0m')
+
+            if not skipped:
+                print('\033[1mDownloading:\033[0m \033[93m' + title + '\033[0m...')
+                while not db_exists(dest + filename + '.pkg') and not db_async_complete(async_id):
+                    kickme()
+                    KICK_TICK = KICK_TICK + 1
                     time.sleep(interval)
-                print('Finished Downloading ' + title)
-                print('File Located at ' + dest + filename + '.pkg')
+                print('\033[1mFinished Downloading:\033[0m \033[92m' + title + '\033[0m')
                 print('Sleeping before next download...')
                 time.sleep(sleep)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='TSV Backup Utility')
@@ -64,16 +93,20 @@ if __name__ == '__main__':
         '-c', dest='path', action='store', required=True,
         help='Path to TSV File')
     parser.add_argument(
-        '--destination', dest='dest', action='store', default='/tsv-backup/', required=False,
+        '--destination', dest='dest', action='store',
+        default='/tsv-backup/', required=False,
         help='Destination path to save files to on Dropbox')
     parser.add_argument(
-        '--interval', dest='interval', action='store', type=int, default='60', required=False,
+        '--interval', dest='interval', action='store', type=int,
+        default='60', required=False,
         help='Number of seconds to wait before checking download status')
     parser.add_argument(
-        '--sleep', dest='sleep', action='store', type=int, default='300', required=False,
+        '--sleep', dest='sleep', action='store', type=int,
+        default='300', required=False,
         help='Number of seconds to wait before starting a new download')
     parser.add_argument(
-        '--kick', dest='kick', action='store', type=int, default='60', required=False,
+        '--kick', dest='kick', action='store', type=int,
+        default='60', required=False,
         help='Number of intervals to wait before kicking the script')
 
     args = parser.parse_args()
@@ -82,4 +115,7 @@ if __name__ == '__main__':
         print('Could not locate TSV file')
         exit()
 
-    main(args.path, args.dest, int(args.interval), int(args.sleep), int(args.kick))
+    KICK_MAX = args.kick
+    KICK_TICK = 0
+
+    main(args.path, args.dest, args.interval, args.sleep)
